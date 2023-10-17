2316dcd5c4da2e8b032b9da0395f2519e7942b90
kutukov2000 <kutukov16.2005@gmail.com>
2023-10-17 21:15:54 +0300

    Add ability view the post

--- SocialNetworkClone/wwwroot/js/site.js
+++ SocialNetworkClone/wwwroot/js/site.js
@@ -2,3 +2,40 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Отримайте модальне вікно
+var modals = document.querySelectorAll(".modal");
+var openModals = document.querySelectorAll(".openModal");
+
+for (var i = 0; i < openModals.length; i++) {
+    openModals[i].onclick = function () {
+        var dataIndex = this.getAttribute("data-index");
+
+        if (modals[dataIndex]) {
+            modals[dataIndex].style.display = "flex";
+        } else {
+            console.error("Modal not found");
+        }
+    };
+}
+
+// Отримайте елементи, які закривають модальне вікно
+var spans = document.querySelectorAll(".close");
+
+// Функція для закриття модального вікна
+for (var i = 0; i < spans.length; i++) {
+    spans[i].onclick = function () {
+        var dataIndex = this.getAttribute("data-index");
+        modals[dataIndex].style.display = "none";
+    };
+}
+
+// Закрийте модальне вікно, якщо користувач клікає поза ним
+window.onclick = function (event) {
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+};
+
